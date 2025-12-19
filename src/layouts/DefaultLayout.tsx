b2799e56d9e0e614be098f060e1c5a1c7418973c
xOmilk <leite.aglo@gmail.com>
2025-12-18 22:27:45 -0300

    feat: adding new layouts to use as default in the project and minor changes

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,3 +1,14 @@
-export function DefaultLayout() {
-    return <></>;
+import { Header } from '../components/Header';
+
+type DefaultLayoutProps = {
+    children?: React.ReactNode;
+};
+
+export function DefaultLayout({ children }: DefaultLayoutProps) {
+    return (
+        <>
+            <Header />
+            <main>{children}</main>
+        </>
+    );
 }
