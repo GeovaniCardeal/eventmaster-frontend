a3eb847a669f000a1b9d5ec98c6eb6a94b389015
xOmilk <leite.aglo@gmail.com>
2026-02-25 16:15:32 -0300

    feat: add ConfigPage component and update AuthRouter for routing

--- src/routes/AuthRouter/index.tsx
+++ src/routes/AuthRouter/index.tsx
@@ -4,19 +4,19 @@ import { Routes } from 'react-router';
 import { LoginPage } from '../../pages/LoginPage';
 import { ForgotPasswordPage } from '../../pages/ForgotPassword';
 import { NotFoundPage } from '../../pages/NotFoundPage';
-import { HomePage } from '../../pages/HomePage';
 import { AreaClientePage } from '../../pages/AreaCliente';
+import { ConfigPage } from '../../pages/ConfigPage';
 
 export function AuthRouter() {
     return (
         <Routes>
             <Route path="register" element={<RegisterPage />}></Route>
             <Route path="login" element={<LoginPage />}></Route>
-            <Route path="HomePage" element={<HomePage />}></Route>
             <Route
                 path="ForgotPassword"
                 element={<ForgotPasswordPage />}
             ></Route>
+            <Route path="config" element={<ConfigPage />} />
             <Route path="AreaCliente" element={<AreaClientePage />}></Route>
             <Route path="*" element={<NotFoundPage />}></Route>
         </Routes>
